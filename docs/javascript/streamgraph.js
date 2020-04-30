c91459723378e0b68a0f496b74674f62458a65c2
Martin <beaussart.martin@protonmail.ch>
2020-04-30 23:02:32 +0200

    add stream graph

--- docs/javascript/streamgraph.js
+++ docs/javascript/streamgraph.js
@@ -1,45 +1,62 @@
 // set the dimensions and margins of the graph
-var margin = {top: 20, right: 30, bottom: 30, left: 60},
-    width = window.innerWidth,
-    height = 600 - margin.top - margin.bottom;
+var margin = {
+    top: 20,
+    right: 30,
+    bottom: 30,
+    left: 60
+  },
+  width = 0.8 * window.innerWidth,
+  height = 690 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg2 = d3.select("#streamgraph")
   .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
   .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+  .attr("transform",
+    "translate(" + margin.left + "," + margin.top + ")");
 
 
 
 
 // Parse the Data
-d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/5_OneCatSevNumOrdered_wide.csv", function(data) {
+//d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/5_OneCatSevNumOrdered_wide.csv", function(data) {
+d3.csv("data/Crimes_by_day_by_type.csv", function(data) {
 
   // List of groups = header of the csv files
   var keys = data.columns.slice(1)
 
   // Add X axis
   var x = d3.scaleLinear()
-    .domain(d3.extent(data, function(d) { return d.year; }))
-    .range([ 0, width ]);
+    .domain(d3.extent(data, function(d) {
+      return d.date;
+    }))
+    .range([0, 650]);
   svg2.append("g")
     .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x).ticks(5));
+    .call(d3.axisBottom(x).tickSize(-height * 1).tickValues([1, 230]))
+    .select(".domain").remove()
+
+  svg2.selectAll(".tick line").attr("stroke", "#f2f2f2")
+
+  // Add X axis label:
+  svg2.append("text")
+    .attr("text-anchor", "end")
+    .attr("x", width / 2)
+    .attr("y", height - 30)
+    .style('fill', '#FAFAFA')
+    .text("2001-2020");
 
   // Add Y axis
   var y = d3.scaleLinear()
-    .domain([-100000, 100000])
-    .range([ height, 0 ]);
-  svg2.append("g")
-    .call(d3.axisLeft(y));
+    .domain([-15000, 25000])
+    .range([height / 4, height]);
 
   // color palette
   var color = d3.scaleOrdinal()
     .domain(keys)
-    .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf'])
+    .range(['#FAFAFA', '#E7E7E9', '#D4D5D9', '#BEBFC5', '#A3A5AD', '#888A95', '#71737F', '#565861'])
 
   //stack the data?
   var stackedData = d3.stack()
@@ -47,17 +64,56 @@ d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_data
     .keys(keys)
     (data)
 
+  // create a tooltip
+  var Tooltip = svg2
+    .append("text")
+    .attr("x", 100)
+    .attr("y", 0)
+    .style("opacity", 0)
+    .style('fill', '#FAFAFA')
+    .style("font-size", 17)
+
+  // Three function that change the tooltip when user hover / move / leave a cell
+  var mouseover = function(d) {
+    Tooltip.style("opacity", 1)
+    d3.selectAll(".myArea").style("opacity", .2)
+    d3.select(this)
+      .style("stroke", "white")
+      .style("opacity", 1)
+  }
+  var mousemove = function(d, i) {
+    grp = keys[i]
+    Tooltip.text(grp)
+  }
+  var mouseleave = function(d) {
+    Tooltip.style("opacity", 0)
+    d3.selectAll(".myArea").style("opacity", 1).style("stroke", "none")
+  }
+
+  // Area generator
+  var area = d3.area()
+    .x(function(d) {
+      return x(d.data.date);
+    })
+    .y0(function(d) {
+      return y(d[0]);
+    })
+    .y1(function(d) {
+      return y(d[1]);
+    })
   // Show the areas
   svg2
     .selectAll("mylayers")
     .data(stackedData)
     .enter()
     .append("path")
-      .style("fill", function(d) { return color(d.key); })
-      .attr("d", d3.area()
-        .x(function(d, i) { return x(d.data.year); })
-        .y0(function(d) { return y(d[0]); })
-        .y1(function(d) { return y(d[1]); })
-    )
+    .attr("class", "myArea")
+    .style("fill", function(d) {
+      return color(d.key);
+    })
+    .attr("d", area)
+    .on("mouseover", mouseover)
+    .on("mousemove", mousemove)
+    .on("mouseleave", mouseleave)
 
 })

92b5f6704948a62406b6ec1a39b759b710091404
ElCabrino <cabrini.vincent@gmail.com>
2020-05-23 17:06:44 +0200

    Add buttons that changes view correctly and clean code

--- docs/javascript/maptest.js
+++ docs/javascript/maptest.js
@@ -1,28 +1,69 @@
-
-//they are shifted from the true location
-var policeIcon = L.icon({
-  iconUrl: 'img/police-icon2.png',
-
-  iconSize:     [50, 50], // size of the icon
-  shadowSize:   [50, 64], // size of the shadow
-  iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-  shadowAnchor: [4, 62],  // the same for the shadow
-  popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-});
-
+var heat;
+var mymap;
+var current_layers = [];
 
 //set map view on Chicago
-var mymap = L.map('mapsection').setView([41.8119, -87.6873], 9);
+mymap = L.map('map').setView([41.8119, -87.6873], 9);
 
-//dark theme
-var theme = L.tileLayer('https://tile.jawg.io/jawg-dark/{z}/{x}/{y}.png?access-token=2NizdI0VAUG9D62DlxhMK39Bs6ls6oqXlsL90NH46qvp5MFKPNEJXxpym8uTO7Dr', {
-        attribution: '<a href="https://www.jawg.io" target="_blank">&copy; Jawg</a> | <a href="https://www.openstreetmap.org" target="_blank">&copy; OpenStreetMap</a>&nbsp;contributors',
-        maxZoom: 22,
-        minZoom: 10,
-      });
+function test() {
+  console.log("OYOYO");
+}
 
-//set the map image
-mymap.addLayer(theme);
+function create_map(){
+  //dark theme
+  var theme = L.tileLayer('https://tile.jawg.io/jawg-dark/{z}/{x}/{y}.png?access-token=2NizdI0VAUG9D62DlxhMK39Bs6ls6oqXlsL90NH46qvp5MFKPNEJXxpym8uTO7Dr', {
+          attribution: '<a href="https://www.jawg.io" target="_blank">&copy; Jawg</a> | <a href="https://www.openstreetmap.org" target="_blank">&copy; OpenStreetMap</a>&nbsp;contributors',
+          maxZoom: 22,
+          minZoom: 10,
+        });
+
+  //set the map image
+  mymap.addLayer(theme);
+
+}
+
+function clear_map(){
+  current_layers.forEach((layer) => {
+    mymap.removeLayer(layer);
+  });
+}
+
+//SHOW POLICE STATION
+function show_police_stations(){
+  d3.csv('data/Police_Stations.csv')
+        .on('load', function (data) {
+
+    clear_map();
+
+    //set the marker
+    data.forEach(function(val) {
+      var stationsMarker = L.marker(
+        [val.LATITUDE, val.LONGITUDE],
+        {icon: policeIcon}).addTo(mymap);
+      current_layers.push(stationsMarker)
+    });
+  }).get();
+}
+
+//SHOW HEATMAP
+function show_heatmap(){
+  //we execute when the csv is open
+  d3.csv('data/heattest.csv')
+        .on('load', function (data) {
+
+    clear_map();
+
+    var t = []
+    //set the marker
+    data.forEach(function(val) {
+      t.push([val.Latitude, val.Longitude, 1])
+    });
+
+    //heatmap plot
+    current_layers.push(L.heatLayer(t, {radius: 25, gradient: {0.4: 'yellow', 0.65: 'orange', 1: 'purple'}}).addTo(mymap));
+
+  }).get();
+}
 
 //AREAS
 
@@ -36,38 +77,11 @@ function style(feature) {
    };
 }
 
-//geojson for area
-L.geoJSON(areas, {style: style}).addTo(mymap);
+function show_areas(){
+  clear_map();
+  //geojson for area
+  current_layers.push(L.geoJSON(areas, {style: style}).addTo(mymap));
+}
 
 
-//MARKERS
-
-//we execute when the csv is open
-d3.csv('data/Police_Stations.csv')
-      .on('load', function (data) {
-
-  //set the marker
-  data.forEach(function(val) {
-    var stationsMaker = L.marker(
-      [val.LATITUDE, val.LONGITUDE],
-      {icon: policeIcon}).addTo(mymap);
-  });
-
-}).get();
-
-//HEATMAP
-
-//we execute when the csv is open
-d3.csv('data/heattest.csv')
-      .on('load', function (data) {
-
-  var t = []
-  //set the marker
-  data.forEach(function(val) {
-    t.push([val.Latitude, val.Longitude, 1])
-  });
-
-  //heatmap plot
-  var heat = L.heatLayer(t, {radius: 25, gradient: {0.4: 'yellow', 0.65: 'orange', 1: 'purple'}}).addTo(mymap);
-
-}).get();
+create_map();

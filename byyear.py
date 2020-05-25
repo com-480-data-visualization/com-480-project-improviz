be33cea24ff2e9c7fb36767b6fd1ce565915ffb9
Luca Joss <luca.joss@hispeed.ch>
2020-05-25 10:39:48 +0200

    Updated version to generate by year, by type crimes

--- byyear.py
+++ byyear.py
@@ -1,22 +1,40 @@
 from datetime import datetime
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+import os
 
 def generateByYearCSV(dataset, dates, year):
 
+    df_date = np.empty(0, dtype=np.dtype('U100'))
+    df_carea = np.empty(0, dtype=np.dtype('i4'))
+    df_type = np.empty(0, dtype=np.dtype('U100'))
+    df_num = np.empty(0, dtype=np.dtype('i4'))
     for d in tqdm(sorted(dates, key=lambda date: datetime.strptime(date, "%m/%d/%Y"))):
         # Isolate year
         date = d.split('/', -1)
         y = date[2]
+        if d == '02/07/2003':
+            print("d_set")
+            print(dataset[d])
         # If the year chages, create .csv and start new csv dataset
         if y == year:
-            for c in dataset[d].keys():
-                c_id = c[0]
-                if (isinstance(dataset[d][c_id], np.int64)):
-                    continue
-                for k in dataset[d][c_id].keys():
-                    df_date.append(d)
-                    df_carea.append(c_id)
-                    df_type.append(k)
-                    df_num.append(dataset[d][c_id][k])    
-csv_name = 'crimes_by_type_year_'+year+".csv"
-final_df = pd.DataFrame({'Date': df_date, 'Community Area': df_carea, 'Primary Type': df_type, 'Number': df_num})
-final_df.to_csv(os.path.join(dataset_path, csv_name))
+            d_arr = np.empty((dataset[d].keys()).size, dtype=np.dtype('U100'))
+            c_arr = np.empty(0, dtype=np.dtype('i4'))
+            t_arr = np.empty(0, dtype=np.dtype('U100'))
+            n_arr = np.empty(0, dtype=np.dtype('i4'))
+            d_arr = np.full_like(d_arr, d)
+            tmp_keys = pd.unique(dataset[d].keys().values.ravel('K'))
+            for tk in tmp_keys:
+                c_id = tk[0]
+                k = tk[1]
+                c_arr = np.insert(c_arr, c_arr.size, c_id)
+                t_arr = np.insert(t_arr, t_arr.size, k)
+                n_arr = np.insert(n_arr, n_arr.size, dataset[d][c_id][k])
+            df_date = np.concatenate((df_date, d_arr), axis=None)
+            df_carea = np.concatenate((df_carea, c_arr), axis=None)
+            df_type = np.concatenate((df_type, t_arr), axis=None)
+            df_num = np.concatenate((df_num, n_arr), axis=None)
+    csv_name = 'crimes_by_type_year'+year+".csv"
+    final_df = pd.DataFrame({'Date': df_date, 'Community Area': df_carea, 'Primary Type': df_type, 'Number': df_num})
+    final_df.to_csv(os.path.join('datasets/', csv_name))

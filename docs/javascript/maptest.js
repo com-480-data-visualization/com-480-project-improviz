58d3c4fed1d00360da663ec3af8d01ebf5ea3f2c
ElCabrino <cabrini.vincent@gmail.com>
2020-05-26 16:26:07 +0200

    animate heatmap with small files

--- docs/javascript/maptest.js
+++ docs/javascript/maptest.js
@@ -3,22 +3,22 @@ var mymap
 var current_layers = []
 
 // variables for heatmap animations
+var heatmap_file_number = 1
 var current_heatmap = null
 var month_heatmap = 1, year_heatmap = 2001
 var interval_heatmap
 
-
 var areas_to_id = {'PORTAGE PARK': 15, 'WEST ENGLEWOOD': 67, 'ENGLEWOOD': 68, 'WASHINGTON PARK': 40, 'HUMBOLDT PARK': 23, 'GRAND BOULEVARD': 38, 'UPTOWN': 3, 'SOUTH SHORE': 43, 'NORTH CENTER': 5, 'NEAR WEST SIDE': 28, 'ALBANY PARK': 14, 'WEST TOWN': 24, 'LOGAN SQUARE': 22, 'NEAR NORTH SIDE': 8, 'NORTH LAWNDALE': 29, 'PULLMAN': 50, 'AUBURN GRESHAM': 71, 'NEW CITY': 61, 'WEST LAWN': 65, 'LOWER WEST SIDE': 31, 'AUSTIN': 25, 'WEST RIDGE': 2, 'EAST GARFIELD PARK': 27, 'KENWOOD': 39, 'DOUGLAS': 35, 'WOODLAWN': 42, 'BELMONT CRAGIN': 19, 'OAKAND': 36, 'ROSELAND': 49, 'LAKEVIEW': 6, 'LOOP': 32, 'NORTH PARK': 13, 'SOUTH DEERING': 51, 'GARFIELD RIDGE': 56, 'BRIDGEPORT': 60, 'LINCOLN SQUARE': 4, 'SOUTH CHICAGO': 46, 'WEST GARFIELD PARK': 26, 'HYDE PARK': 41, 'NEAR SOUTH SIDE': 33, 'ROGERS PARK': 1, 'MONTCLARE': 18, 'WEST PULLMAN': 53, 'AVALON PARK': 45, 'CHICAGO LAWN': 66, 'EDGEWATER': 77, 'WASHINGTON HEIGHTS': 73, 'HEGEWISCH': 55, 'SOUTH LAWNDALE': 30, 'GAGE PARK': 63, 'CHATHAM': 44, 'WEST ELSDON': 62, 'AVONDALE': 21, 'FULLER PARK': 37, 'GREATER GRAND CROSSING': 69, 'ASHBURN': 70, 'IRVING PARK': 16, 'RIVERDALE': 54, 'NORWOOD PARK': 10, 'JEFFERSON PARK': 11, 'BRIGHTON PARK': 58, 'DUNNING': 17, 'LINCOLN PARK': 7, 'EDISON PARK': 9, 'FOREST GLEN': 12, 'HERMOSA': 20, 'ARMOUR SQUARE': 34, 'BURNSIDE': 47, 'CALUMET HEIGHTS': 48, 'EAST SIDE': 52, 'ARCHER HEIGHTS': 57, 'MCKINLEY PARK': 59, 'CLEARING': 64, 'BEVERLY': 72, 'MOUNT GREENWOOD': 74, 'MORGAN PARK': 75, 'OHARE': 76}
 
-function startLoadOverlay() {
-	document.getElementById("load_overlay").style.display = "block";
+function startLoadOverlay () {
+  document.getElementById('load_overlay').style.display = 'block'
 }
 
-function stopLoadOverlay() {
-	//var i = document.getElementById("lgif");
-	//i.style.display = "none";
-	//console.log("hide gif");
-	document.getElementById("load_overlay").style.display = "none";
+function stopLoadOverlay () {
+	// var i = document.getElementById("lgif");
+	// i.style.display = "none";
+	// console.log("hide gif");
+  document.getElementById('load_overlay').style.display = 'none'
 }
 
 // set map view on Chicago
@@ -51,33 +51,69 @@ function clear_map () {
 }
 
 // -------------- DISPLAY HEATMAPS POLICE STATIONS ---------------------
-function display_heatmap (data) {
-  console.log(year_heatmap)
-  console.log(month_heatmap)
-  if (year_heatmap == 2020) {
-    console.log('should stop')
-    clearInterval(interval_heatmap)
-  } else {
-    if (current_heatmap != null) {
-      mymap.removeLayer(current_heatmap)
-    }
-    current_heatmap = L.heatLayer(data[year_heatmap.toString()][month_heatmap.toString()], {radius: 25, gradient: {0.4: 'yellow', 0.65: 'orange', 1: 'purple'}}).addTo(mymap)
-    month_heatmap = (month_heatmap + 1) % 13
-    if (month_heatmap == 0) {
-      month_heatmap = 1
-      year_heatmap++
-    }
+function display_one_heatmap (data) {
+  if (current_heatmap != null) {
+    mymap.removeLayer(current_heatmap)
+  }
+  current_heatmap = L.heatLayer(data[year_heatmap.toString()][month_heatmap.toString()], {radius: 25, gradient: {0.4: 'yellow', 0.65: 'orange', 1: 'purple'}}).addTo(mymap)
+  month_heatmap = (month_heatmap + 1) % 13
+  if (month_heatmap == 0) {
+    month_heatmap = 1
+    year_heatmap++
   }
 }
 
-function animate_heatmaps (data) {
-  console.log('dataset heatmaps loaded')
-  interval_heatmap = setInterval(display_heatmap, 10, data)
+function display_heatmap (data) {
+  console.log(year_heatmap)
+  console.log(month_heatmap)
+
+  display_one_heatmap(data)
+
+  if ((year_heatmap == 2006) && (month_heatmap==1)) {
+		console.log("change to 2006")
+    clearInterval(interval_heatmap)
+		startLoadOverlay()
+    animate_heatmaps('data/heatmap_2006_2010.json')
+    return
+  } if ((year_heatmap == 2011)  && (month_heatmap==1)){
+		console.log("change to 2011")
+    clearInterval(interval_heatmap)
+		startLoadOverlay()
+    animate_heatmaps('data/heatmap_2011_2015.json')
+    return
+  } if ((year_heatmap == 2016)&& (month_heatmap==1)){
+		console.log("change to 2016")
+    clearInterval(interval_heatmap)
+		startLoadOverlay()
+    animate_heatmaps('data/heatmap_2016_2020.json')
+    return
+  } else if (year_heatmap == 2020) {
+    console.log('should stop')
+    clearInterval(interval_heatmap)
+    return
+  }
+}
+
+// function animate_heatmaps (data) {
+//   console.log('dataset heatmaps loaded')
+//   interval_heatmap = setInterval(display_heatmap, 10, data)
+// }
+
+function animate_heatmaps (filepath) {
+	console.log("animate:")
+	console.log(filepath)
+	console.log("interval heatmap")
+	console.log(interval_heatmap)
+  d3.json(filepath, function (data) {
+		console.log("file loaded")
+    stopLoadOverlay()
+    interval_heatmap = setInterval(display_heatmap, 10, data)
+  })
 }
 
 // function called by button
 function show_police_stations () {
-  startLoadOverlay();
+  startLoadOverlay()
   clear_map()
 
   var layers
@@ -96,19 +132,14 @@ function show_police_stations () {
   })
 
   console.log('police ok')
-
-  d3.json('data/big_data/heatmap_json.json', function (data) {
-    // setTimeout(animate_heatmaps, 1, data);
-    stopLoadOverlay();
-    animate_heatmaps(data)
-  })
+  animate_heatmaps('data/heatmap_2001_2005.json')
 }
 
 // -------------- END HEATMAPS POLICE STATIONS ---------------------
 
 // SHOW HEATMAP
 function show_heatmap () {
-  startLoadOverlay();
+  startLoadOverlay()
   // we execute when the csv is open
   d3.csv('data/heattest.csv')
         .on('load', function (data) {
@@ -122,14 +153,14 @@ function show_heatmap () {
 
     // heatmap plot
           current_layers.push(L.heatLayer(t, {radius: 25, gradient: {0.4: 'yellow', 0.65: 'orange', 1: 'purple'}}).addTo(mymap))
-	  stopLoadOverlay();
+	  stopLoadOverlay()
         }).get()
 }
 
 // AREAS
 
 function show_areas () {
-  startLoadOverlay();
+  startLoadOverlay()
   filename = 'data/crimes_by_type_year_2002.csv'
   type = 'OFFENSE INVOLVING CHILDREN'
   date = '01/01/2002'
@@ -165,7 +196,7 @@ function show_areas () {
     }
 
     current_layers.push(L.geoJSON(areas, {style: style}).addTo(mymap))
-    stopLoadOverlay();
+    stopLoadOverlay()
   }).get()
 }
 

47f9fcc81047cdc01a70e708e25144967f964608
Luca Joss <luca.joss@hispeed.ch>
2020-05-27 09:50:48 +0200

    Removed date generation code for date selection

--- docs/javascript/maptest.js
+++ docs/javascript/maptest.js
@@ -293,51 +293,6 @@ function show_sdates () {
   })
 }
 
-var monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-var monthDaysBis = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
-for (var y = 2001; y <= 2019; y++) {
-  for (var m = 1; m <= 12; m++) {
-    if (y % 4 == 0) {
-      for (d = 1; d <= monthDaysBis[m - 1]; d++) {
-        if (d < 10) {
-          var day = '0' + d.toString(10)
-        } else {
-          var day = d.toString(10)
-        }
-        if (m < 10) {
-          var month = '0' + m.toString(10)
-        } else {
-          var month = m.toString(10)
-        }
-        var val = day + '/' + month + '/' + y.toString(10)
-        var option = document.createElement('option')
-        option.value = val
-        option.text = val
-        selectDate.appendChild(option)
-      }
-    } else {
-      for (d = 1; d <= monthDays[m - 1]; d++) {
-        if (d < 10) {
-          var day = '0' + d.toString(10)
-        } else {
-          var day = d.toString(10)
-        }
-        if (m < 10) {
-          var month = '0' + m.toString(10)
-        } else {
-          var month = m.toString(10)
-        }
-        var val = day + '/' + month + '/' + y.toString(10)
-        var option = document.createElement('option')
-        option.value = val
-        option.text = val
-        selectDate.appendChild(option)
-      }
-    }
-  }
-}
-
 d3.json('data/types_json.json', function (data) {
   var t = data['types']
   for (i = 0; i < t.length; i++) {
